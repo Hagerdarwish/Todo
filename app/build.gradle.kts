c0cbfcf7db6628cd19381e3ffbda9b78bc19abdb
Hagerdarwish <hagardarwish.s@gmail.com>
2024-02-15 09:29:44 +0200

    Add calender view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
     kapt ("androidx.room:room-compiler:$room_version")
+    implementation ("com.github.prolificinteractive:material-calendarview:2.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
